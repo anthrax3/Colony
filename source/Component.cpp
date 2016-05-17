1964970b121b5161ba590a15932b53bcc840cb6a
mateuszmidor <kontakt@mateuszmidor.com>
2016-05-17 21:09:11 +0200

    Added class Component

---
+++ source/Component.cpp
@@ -0,0 +1,18 @@
+/**
+ *   @file: Component.cpp
+ *
+ *   @date: May 17, 2016
+ * @author: Mateusz Midor
+ */
+
+#include "Component.h"
+
+Component::Component() {
+    // TODO Auto-generated constructor stub
+
+}
+
+Component::~Component() {
+    // TODO Auto-generated destructor stub
+}
+
---
+++ source/Component.h
@@ -0,0 +1,17 @@
+/**
+ *   @file: Component.h
+ *
+ *   @date: May 17, 2016
+ * @author: Mateusz Midor
+ */
+
+#ifndef SOURCE_COMPONENT_H_
+#define SOURCE_COMPONENT_H_
+
+class Component {
+public:
+    Component();
+    virtual ~Component();
+};
+
+#endif /* SOURCE_COMPONENT_H_ */
