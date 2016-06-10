3e87248f264e11b94b11897c39081ffd10a536b5
mateusz <mateusz@mateusz-VirtualBox>
2016-06-10 11:03:46 +0200

    Added install_opengl.sh and setup_qt_cmake.sh

---
+++ install_opengl.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt-get install mesa-common-dev -y
+sudo apt-get install libglu1-mesa-dev -y
---
+++ setup_qt_cmake.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export CMAKE_PREFIX_PATH=/usr/local/lib/Qt/5.5/gcc_64
